fec151e1d78125a797921239bcc811275c95be77
Xiaorui <xiaoruipearl@gmail.com>
2017-09-30 17:45:40 -0500

    added three test cases

--- mp4/src/test/java/edu/illinois/cs427/mp4/BookTest.java
+++ mp4/src/test/java/edu/illinois/cs427/mp4/BookTest.java
@@ -6,13 +6,19 @@ import static junit.framework.Assert.assertEquals;
 
 public class BookTest extends TestCase{
     public void testBookConstructor1() {
-        Book book1 = new Book("hello", "world");
-        assertEquals("hello", book1.getTitle());
-        assertEquals("world", book1.getAuthor());
+      Book book1 = new Book("hello", "world");
+      assertEquals("hello", book1.getTitle());
+      assertEquals("world", book1.getAuthor());
     }
 
+    public void testBookConstructor2() {
+      Book book2 = new Book("{\"title\":\"bo, ok2\",\"author(s)\":\"pe, arl\"}");
+      assertEquals("bo, ok2", book2.getTitle());
+      assertEquals("pe, arl", book2.getAuthor());
+    }
     public void testGetStringRepresentation1() {
-        //TODO implement this
+        Book book1 = new Book("hello", "world");
+        assertEquals("{\"title\":\"hello\",\"author(s)\":\"world\"}",book1.getStringRepresentation());
     }
 
     public void testGetContainingCollections1() {
